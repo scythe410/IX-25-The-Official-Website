85bb2bec8f25a7b539b1f77edfb96b0143d2d655
Dimesh Anthoney <dimeshmendis123@gmail.com>
2025-10-08 14:36:52 +0000

    add behind the marquee strip a strip in blue which shows little behind

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 const Marquee = () => {
   const marqueeText = "IX '25 • ";
   return (
-    <div className="relative flex overflow-x-hidden bg-pink-500 text-black py-4 my-12 -rotate-3 font-bebas">
-      <div className="flex animate-marquee whitespace-nowrap">
-        <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
-        <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
+    <div className="relative my-12 -rotate-3 font-bebas">
+      <div className="absolute inset-0 bg-blue-300 transform -rotate-1"></div>
+      <div className="relative flex overflow-x-hidden bg-pink-500 text-black py-4">
+        <div className="flex animate-marquee whitespace-nowrap">
+          <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
+          <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
+        </div>
       </div>
     </div>
   );
