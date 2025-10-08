03a0478c730ad54e1eaa0413ec7f5969bd001880
Dimesh Anthoney <dimeshmendis123@gmail.com>
2025-10-08 14:32:46 +0000

    text is inconsistent it has gone upper in some sections

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -4,10 +4,8 @@ const Marquee = () => {
   const marqueeText = "IX '25 • ";
   return (
     <div className="relative flex overflow-x-hidden bg-pink-500 text-black py-4 my-12 -rotate-3 font-bebas">
-      <div className="animate-marquee whitespace-nowrap">
+      <div className="flex animate-marquee whitespace-nowrap">
         <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
-      </div>
-      <div className="absolute top-0 animate-marquee2 whitespace-nowrap">
         <span className="text-4xl mx-4">{marqueeText.repeat(15)}</span>
       </div>
     </div>
