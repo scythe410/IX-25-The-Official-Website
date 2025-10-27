fe48f5092e44e791e05e1a4aba85ad062817fdcf
Dimesh Anthoney <dimeshmendis123@gmail.com>
2025-10-27 16:18:55 +0000

    Hydration failed because the server rendered HTML didn't match the clien

---
+++ src/components/PageClientEffects.tsx
@@ -0,0 +1,24 @@
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import Preloader from '@/components/Preloader';
+
+export default function PageClientEffects() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <>{loading && <Preloader />}</>;
+}
+
+    
