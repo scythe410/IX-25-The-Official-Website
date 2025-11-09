271e51be47d489592bd2fa7bfe5ae7be82d1ba3a
Dimesh Anthoney <dimeshmendis123@gmail.com>
2025-11-09 17:29:12 +0000

    maka a dotted line along the marquee strip

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,10 +1,11 @@
+
 import React from 'react';
 
 const Marquee = ({ text }: { text: string }) => {
   const marqueeText = `${text} • `.repeat(15);
   return (
     <div className="relative my-12 -rotate-3">
-      <div className="relative flex overflow-x-hidden bg-[#FF0879] text-black py-4">
+      <div className="relative flex overflow-x-hidden bg-[#FF0879] text-black py-4 border-y-2 border-dotted border-black/50">
         <div className="flex animate-marquee whitespace-nowrap">
           <span className="text-4xl mx-4">{marqueeText}</span>
           <span className="text-4xl mx-4">{marqueeText}</span>
